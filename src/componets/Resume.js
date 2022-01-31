d1a45e2d07beb1f28816d3a0055dc9d21fd46819
panupong mikada <path301242@gmail.com>
2022-01-31 12:18:08 +0700

    เริ่มวาง project

---
+++ src/componets/Resume.js
@@ -0,0 +1,9 @@
+function Resume(){
+    return (
+        <h1 className="text-3xl font-bold">
+            Hello world!
+        </h1>
+    )
+}
+
+export default Resume
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Resume from './componets/Resume.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Resume />
   </React.StrictMode>,
   document.getElementById('root')
 );
