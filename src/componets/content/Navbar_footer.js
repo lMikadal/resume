bff20f583ea9fee63fffcbf4b8241e45c4a54a27
panupong mikada <path301242@gmail.com>
2022-02-02 17:49:34 +0700

    update navbar(footer)

--- src/componets/content/Navbar_footer.js
+++ src/componets/content/Navbar_footer.js
@@ -1,13 +1,20 @@
-import './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faLine } from "@fortawesome/free-brands-svg-icons"
 
-function NavFooterItem(props) {
-    const { item } = props;
-
+function NavFooter() {
     return (
-        <a href='#' className='block py-2.5 px-4 rounded-md font-bold hover:bg-slate-500'>
-            {item}
-        </a> 
+        <div className="text-4xl flex justify-between px-8 absolute inset-x-0 bottom-3">
+            <a href="https://github.com/lMikadal" target="_blank">
+                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+            </a> 
+            <a href="https://www.facebook.com/panupong.mikada/" target="_blank">
+                <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>  
+            </a> 
+            <a>
+                <FontAwesomeIcon icon={faLine}></FontAwesomeIcon>  
+            </a>
+        </div>
     );
 }
 
-export default NavFooterItem;
+export default NavFooter;
